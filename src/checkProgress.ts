6f820d7985d88a3648db3c64b9a3fbf99c5b706a
andrew840513 <andrew@ionblaster.com>
2024-04-09 02:04:32 -0700

    fix file not exists issue

--- src/checkProgress.ts
+++ src/checkProgress.ts
@@ -6,8 +6,13 @@ interface StoredData {
     statusText: string;
 }
 async function checkProgress(): Promise<void> {
-    const storedData = await Bun.file('data.json').json();
-
+    let storedData = Bun.file('data.json');
+    if (await storedData.exists()) {
+        storedData = await storedData.json();
+    } else {
+        await Bun.write('data.json', JSON.stringify({dateText:'', statusText:''}));
+        storedData = await Bun.file('data.json').json();
+    }
     let browser: Browser | null = null;
     let context: BrowserContext | null = null;
     let page: Page | null = null;
